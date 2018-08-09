ef0af06de73c71e5087ddb445ca38186e30996a1
jsunwoo <jinsung.sunwoo@gmail.com>
2018-08-09 18:16:45 +0900

    Change circle color by changing state

--- ToDo.js
+++ ToDo.js
@@ -16,10 +16,16 @@ export default class ToDo extends Component {
   };
 
   render() {
+    const { isCompleted } = this.state;
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={this._toggleComplete}>
-          <View style={styles.circle} />
+          <View
+            style={[
+              styles.circle,
+              isCompleted ? styles.completedCircle : styles.uncompletedCircle
+            ]}
+          />
         </TouchableOpacity>
         <Text style={styles.text}>Hello I'm a To Do</Text>
       </View>
@@ -29,7 +35,7 @@ export default class ToDo extends Component {
   _toggleComplete = () => {
     this.setState(prevState => {
       return {
-          isCompleted: !prevState.isCompleted
+        isCompleted: !prevState.isCompleted
       };
     });
   };
@@ -46,11 +52,16 @@ const styles = StyleSheet.create({
   circle: {
     width: 30,
     height: 30,
-    borderRadius: 15,
     // borderRadius 는 크기는 항상 반지름
-    borderColor: "red",
+    borderRadius: 15,
     borderWidth: 3,
     marginRight: 15
+  },
+  completedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#F23657"
   },
   text: {
     fontWeight: "200",
