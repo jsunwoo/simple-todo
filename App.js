55f22723119b71d669598eb45df14f5f31262400
jsunwoo <jinsung.sunwoo@gmail.com>
2018-09-16 16:00:26 +0900

    Fix bug occuring from inserted null to Object.values(toDos)

--- App.js
+++ App.js
@@ -90,7 +90,7 @@ export default class App extends React.Component {
       const parsedToDos = JSON.parse(toDos);
       this.setState({
         loadedToDos: true,
-        toDos: parsedToDos
+        toDos: parsedToDos || {}
       });
     } catch (err) {
       console.log(err);
